09669a00b9bb6efd11f29c3ace110051d9e243b5
chaplygin_anton <chaplygin.anton@gmail.com>
2019-05-23 12:00:40 +0300

    initial commit

---
+++ includes/class-cupon-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       www.example.com
+ * @since      1.0.0
+ *
+ * @package    Cupon
+ * @subpackage Cupon/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Cupon
+ * @subpackage Cupon/includes
+ * @author     SerfCompany <example@example.com>
+ */
+class Cupon_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-cupon-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       www.example.com
+ * @since      1.0.0
+ *
+ * @package    Cupon
+ * @subpackage Cupon/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Cupon
+ * @subpackage Cupon/includes
+ * @author     SerfCompany <example@example.com>
+ */
+class Cupon_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
